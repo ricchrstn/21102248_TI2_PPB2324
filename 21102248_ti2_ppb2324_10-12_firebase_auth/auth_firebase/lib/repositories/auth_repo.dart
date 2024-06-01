bfb6abe1eac91bd6bdc4b6124856114ff67eec7a
ricchrstn <rickychristian2305@gmail.com>
2024-06-01 14:51:50 +0700

    DONE

---
+++ 21102248_ti2_ppb2324_10-12_firebase_auth/auth_firebase/lib/repositories/auth_repo.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepo {
+  final _auth = FirebaseAuth.instance;
+  Future<void> login({required String email, required String password}) async {
+    try {
+      final user = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseException catch (e) {
+      throw e.message ?? 'Something wrong!';
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> register(
+      {required String email, required String password}) async {
+    try {
+      final user = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseException catch (e) {
+      throw e.message ?? 'Something wrong!';
+    } catch (e) {
+      throw e;
+    }
+  }
+}
--- 21102248_ti2_ppb2324_10-12_firebase_auth/firebase_auth10_12/android/app/src/main/kotlin/com/example/firebase_auth10_12/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.firebase_auth10_12
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
